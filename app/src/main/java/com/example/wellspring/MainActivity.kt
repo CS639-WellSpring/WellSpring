4f39071824ed63b93148f325c4f1bbf7a73da218
zaidzr3 <zaidakhtar313@gmail.com>
2024-04-27 12:09:10 -0400

    Adding Sign-In Page

---
+++ app/src/main/java/com/example/wellspring/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.wellspring
+
+import com.example.wellspring.ui.theme.WellspringUITheme
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WellspringUITheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                        SignInView()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WellspringUITheme {
+        SignInView()
+    }
+}
