081b9cf9f8b593c538c3e3f3f4a81fb652ae7fd5
akbarmohammed982 <akbarmohammed8465@gmail.com>
2024-05-02 10:45:37 -0400

    Updated Files

---
+++ WellSpring-main/app/src/main/java/com/example/wellspring/ui/theme/Color.kt
@@ -0,0 +1,33 @@
+package com.example.wellspring.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Excited = Color(0xFFFFA340)
+val Happy = Color(0xFFFFD65D)
+val Peace = Color(0xFF3CCD4C)
+val Bored = Color(0xFFB9C5C1)
+val Worried = Color(0xFFC4A8C7)
+val Sad = Color(0xFF45679E)
+
+val bar = Color(0xFFFFD7E3)
+
+object Color {
+    val primary = Color(0xFF6750A4)
+    val onPrimary = Color(0xFFFFFFFF)
+    val primaryContainer = Color(0xFFEADDFF)
+    val onPrimaryContainer = Color(0xFF21005D)
+    val secondary = Color(0xFF625B71)
+    val onSecondary = Color(0xFFFFFFFF)
+    val secondaryContainer = Color(0xFFE8DEF8)
+    val onSecondaryContainer = Color(0xFF1D192B)
+
+    val primaryDark = Color(0xFFD0BCFF)
+    val onPrimaryDark = Color(0xFF381E72)
+    val primaryContainerDark = Color(0xFF4F378B)
+    val onPrimaryContainerDark = Color(0xFFEADDFF)
+    val secondaryDark = Color(0xFFCCC2DC)
+    val onSecondaryDark = Color(0xFF332D41)
+    val secondaryContainerDark = Color(0xFF4A4458)
+    val onSecondaryContainerDark = Color(0xFFE8DEF8)
+}
+
---
+++ WellSpring-main/app/src/main/java/com/example/wellspring/ui/theme/Type.kt
@@ -0,0 +1,7 @@
+package com.example.wellspring.ui.theme
+
+import androidx.compose.material3.Typography
+
+// Using default Material You typography
+val Typography = Typography()
+
